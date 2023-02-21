e5f9e27b79945c945e7b02baa20e6fc7c4b80f43
Chalana02 <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-02-21 08:33:42 +0530

    Update db.php

--- BackEnd_DiaTracker/DatabaseComponent/save_data/db.php
+++ BackEnd_DiaTracker/DatabaseComponent/save_data/db.php
@@ -1,10 +1,10 @@
 <?php
-    // Enter your host name, database username, password, and database name.
+    // Enter  host name, database username, password, and database name.
     $servername = "localhost";
     $username = "username";
     $password = "";
     $dbname = "LoginSystem";
-    // If you have not set database password on localhost then set empty.
+    
     $con = mysqli_connect("localhost","root","","LoginSystem");
     // Check connection
     if (mysqli_connect_errno()){
