ae9a5c3028d5f8fa95f2a059cb595a97ed669106
Methma Lochanie <methmalochanie1119@gmail.com>
2023-02-24 00:55:27 +0530

    Files changed

--- FrontEnd_DiaTracker/react-script/src/App.js
+++ FrontEnd_DiaTracker/react-script/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './css/loading.css'
-import GetStart from './Pages/GetStart';
+import GetStarted from './Pages/GetStarted';
+
 
 function App() {
   return (
     <div className = "App">
-      <GetStart/>
+      <GetStarted/>
     </div>
   );
 }
