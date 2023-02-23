c21ee6bf234d61d29507b3f0e98d14fefcb13205
Methma Lochanie <methmalochanie1119@gmail.com>
2023-02-24 01:23:55 +0530

    All files deleted

--- FrontEnd_DiaTracker/react-script/src/App.js
+++ FrontEnd_DiaTracker/react-script/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import GetStarted from './Pages/GetStarted';
+
 
 
 function App() {
   return (
     <div className = "App">
-      <GetStarted/>
+      
     </div>
   );
 }
