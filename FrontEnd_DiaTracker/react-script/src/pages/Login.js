ef5af2340eab82e518b44b7bed92ff78926cc035
Methma Lochanie <methmalochanie1119@gmail.com>
2023-02-25 16:13:14 +0530

    Files updated

--- FrontEnd_DiaTracker/react-script/src/pages/Login.js
+++ FrontEnd_DiaTracker/react-script/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './login.css'
+import  '../Components/login.css'
 
 export default function Login() {
   return (
--- FrontEnd_DiaTracker/react-script/src/pages/Start.js
+++ FrontEnd_DiaTracker/react-script/src/pages/Start.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import Description from '../Components/Description'
 import Navigation from '../Components/Navigation'
+import Login from './Login.js'
+
+import '../Components/description.css'
+import VectorIllustrations from '../Components/DrawKit Vector Illustration Health & Medical (3).png'
 
 export default function Start() {
   return (
     <>
         <Navigation />
-        <Description />
+        <div class="head">
+            <div class="head-img">
+                <img src = {VectorIllustrations} alt="DrawKit Vector Illustration Health & Medical" />
+            </div>
+            <div class="head-text">
+                <h1>One stop for all your diabetic needs</h1>
+                <p>DiaTracker is built with the sole purpose of providing functionalities for
+                    the ease of diabetes patients in managing diabetes with one single app </p>
+            </div>
+            <div class="head-btn">
+                <a href = {'Login'}>Get started</a>
+            </div>
+
+        </div>
+        
     </>
   )
 }
