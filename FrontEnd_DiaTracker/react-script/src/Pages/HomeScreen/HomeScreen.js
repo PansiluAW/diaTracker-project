9af3b60546403784041bc9877007f9e0b293077c
Methma Lochanie <methmalochanie1119@gmail.com>
2023-03-14 23:20:50 +0530

    HomeScreen Updated

--- FrontEnd_DiaTracker/react-script/src/Pages/HomeScreen/HomeScreen.js
+++ FrontEnd_DiaTracker/react-script/src/Pages/HomeScreen/HomeScreen.js
@@ -3,21 +3,18 @@ import React from "react";
 import HomeLeftPane from "../../Components/HomeLeftPane/HomeLeftPane";
 import FoodRecomPane from "../../Components/FoodRecompane/FoodRecomPane";
 import ExerciseRecomPane from "../../Components/ExerciseRecomPane/ExerciseRecomPane";
-import "./HomeScreen.css"
+import "./HomeScreen.css";
 // import { Line } from "react-chartjs-2";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "../../Components/Navigation/NavBar";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function HomeScreen() {
-
-
-
   // window.addEventListener("inputCollect", () => {
   //   const loader = document.querySelector(".loader");
-  
+
   //   loader.classList.add("loader--hidden");
-  
+
   //   loader.addEventListener("transitionend", () => {
   //     document.body.removeChild(loader);
   //   });
@@ -25,22 +22,24 @@ export default function HomeScreen() {
 
   return (
     <>
-    <div className = "container-fluid pt-2 pb-5 homebod">
-    <NavBar/>
-      <div className = "row">
-        <div className = "col">
-          <HomeLeftPane/>
-        </div>
-        <div className = "col recom">
-          <div className = "col foodrec">
-          <FoodRecomPane/>
+      <Router>
+        <div className="container-fluid pt-2 pb-5 homebod">
+          <NavBar />
+          <div className="row">
+            <div className="col">
+              <HomeLeftPane />
+            </div>
+            <div className="col recom">
+              <div className="col foodrec">
+                <FoodRecomPane />
+              </div>
+              <div className="col excrec">
+                <ExerciseRecomPane />
+              </div>
+            </div>
           </div>
-          <div className = "col excrec">
-          <ExerciseRecomPane/>
-          </div>
         </div>
-      </div>
-    </div>
+      </Router>
     </>
   );
 }
