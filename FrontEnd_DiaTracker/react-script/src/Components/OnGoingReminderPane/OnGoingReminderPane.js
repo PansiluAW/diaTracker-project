c0670c0aa162593194e4354d782be13fff1c4f35
Methma Lochanie <methmalochanie1119@gmail.com>
2023-03-14 23:28:18 +0530

    OnGoingPane component added

---
+++ FrontEnd_DiaTracker/react-script/src/Components/OnGoingReminderPane/OnGoingReminderPane.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function OnGoingReminderPane(props) {
+  const updated = props.mediName;
+  const value1 = props.time;
+
+  const [isDelete, setDelete] = useState(true);
+
+  const deleteInput = () => {
+    setDelete(false);
+  };
+  return (
+    <>
+      
+      {isDelete ? (
+        <><h3>On-Going Reminders</h3><div>
+          {updated}
+          <span className="vl"></span>
+
+          {value1}
+          <span className="vl"></span>
+          {/* <DeleteIcon className="deleteIcon" onClick={deleteInput} /> */}
+          <DeleteIcon className="deleteIcon" onClick={deleteInput} />
+        </div></>
+      ) : null}
+    </>
+  );
+}
