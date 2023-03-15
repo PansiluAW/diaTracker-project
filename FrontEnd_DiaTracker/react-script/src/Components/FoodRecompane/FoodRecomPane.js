ca8e0da1fe075ffbdde5696f3a0d29616b680b70
PansiluAW <pansiluwijesiri@gmail.com>
2023-03-15 21:16:34 +0530

    Update FoodRecomPane.js

--- FrontEnd_DiaTracker/react-script/src/Components/FoodRecompane/FoodRecomPane.js
+++ FrontEnd_DiaTracker/react-script/src/Components/FoodRecompane/FoodRecomPane.js
@@ -4,20 +4,20 @@ import "./FoodRecomPane.css"
 export default function FoddRecomPane() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+//   useEffect(() => {
+//     getData();
+//   }, []);
 
-  const getData = () => {
-    axios
-      .get("#")
-      .then((res) => res.data)
-      .then((r) => setData(r));
-  };
-for (let i = 0; i < data.length; i++) {
-  const element = array[i];
+//   const getData = () => {
+//     axios
+//       .get("#")
+//       .then((res) => res.data)
+//       .then((r) => setData(r));
+//   };
+// for (let i = 0; i < data.length; i++) {
+//   const element = array[i];
   
-}
+// }
   return (
     <div className="foodRecomPane">
       Food Recoomendation
