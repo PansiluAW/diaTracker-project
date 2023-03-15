ec360906fbcb1f8dfdd7c0600c230a3f49e5ead5
Methma Lochanie <methmalochanie1119@gmail.com>
2023-03-15 19:05:51 +0530

    Line Chart Done

--- FrontEnd_DiaTracker/react-script/src/Components/FoodRecompane/FoodRecomPane.js
+++ FrontEnd_DiaTracker/react-script/src/Components/FoodRecompane/FoodRecomPane.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./FoodRecomPane.css"
 
 export default function FoddRecomPane() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get("#")
+      .then((res) => res.data)
+      .then((r) => setData(r));
+  };
+for (let i = 0; i < data.length; i++) {
+  const element = array[i];
+  
+}
   return (
     <div className="foodRecomPane">
       Food Recoomendation
