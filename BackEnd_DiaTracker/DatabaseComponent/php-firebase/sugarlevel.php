f5dc1112228dd2e634d415aa22f853ebfd2bd529
ChalanaSangeeth123 <chalana.20210659@iit.ac.lk>
2023-03-18 07:42:17 +0530

    delete sugarlevel.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/sugarlevel.php
+++
@@ -1,9 +0,0 @@
-<form id="add-sugar-level-form" action="insert_data.php" method="POST">
-  <label>Date:</label>
-  <input type="date" name="date" required>
-  <label>Time:</label>
-  <input type="time" name="time" required>
-  <label>Sugar Level:</label>
-  <input type="number" name="value" required>
-  <button type="submit">Add Sugar Level</button>
-</form>
