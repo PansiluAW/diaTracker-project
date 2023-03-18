20660932535ad9c35ed967414ac806eeba2f8045
ChalanaSangeeth123 <chalana.20210659@iit.ac.lk>
2023-03-18 07:55:40 +0530

    update logout.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php
+++ BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php
@@ -4,7 +4,7 @@ session_start();
 unset($_SESSION['verified_user_id']);
 unset($_SESSION['idTokenString']);
 
-$_SESSION['status'] = "Logged Out Successfully";
+$_SESSION['status'] = "<div class='error'>Logged Out Successfully</div>";
 header("Location: login.php");
 exit();
 ?>
