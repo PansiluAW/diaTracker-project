9ff8774e2766eba54ccb6da7434ae778149ee342
Rakshaagar <124656658+Rakshaagar@users.noreply.github.com>
2023-03-22 00:32:24 +0530

    Add files via upload

---
+++ FrontEnd_DiaTracker/About.js
@@ -0,0 +1,22 @@
+function fadeInOnScroll() {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach(element => {
+      if (isElementInViewport(element)) {
+        element.classList.remove('fade-in');
+      }
+    });
+  }
+  
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  window.addEventListener('scroll', fadeInOnScroll);
+
+  window.addEventListener('load', fadeInOnScroll);
