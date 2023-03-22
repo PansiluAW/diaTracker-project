19c8f63babb1038205588162c7d34142943de33b
PansiluAW <pansiluwijesiri@gmail.com>
2023-03-22 17:00:27 +0530

    Update get-sugar-levels.php

--- BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/get-sugar-levels.php
+++ BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/get-sugar-levels.php
@@ -9,12 +9,16 @@ include('database_connection.php');
         $SQL_SELECT = "SELECT added_date, sugar_data  FROM (SELECT added_date, sugar_data, ROW_NUMBER() OVER (ORDER BY id DESC) AS row_num FROM hello) AS temp_table WHERE row_num <= 10 ORDER BY row_num DESC";
 
         $execSQL = mysqli_query($conn,$SQL_SELECT);
-
         if ($execSQL){
             $data = array();
             $data[] = ["x", "Suger Level"];
-            while($row = mysqli_fetch_array($execSQL)){
-                $data[] = array($row['added_date'], (int)$row['sugar_data']);
+            $numRows = mysqli_num_rows($execSQL);
+            if($numRows>0){
+                while($row = mysqli_fetch_array($execSQL)){
+                    $data[] = array($row['added_date'], (int)$row['sugar_data']);
+                }
+            }else{
+                $data[] = [0,0];
             }
             echo json_encode($data);  
         }else{
@@ -22,7 +26,6 @@ include('database_connection.php');
             echo 'alert("Urgh...an unexpected error occured")';
             echo '</script>';
         }
-
         // $arrayp = mysqli_fetch_array($execSQL);  
         // echo json_encode($data);
     }
