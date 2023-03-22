0b5e9d269e640b00b8649498fd3a127c91006577
Methuu1119 <104524747+Methuu1119@users.noreply.github.com>
2023-03-22 21:45:13 +0530

    Exercise recommendaded pane update

--- FrontEnd_DiaTracker/react-script/src/Components/ExerciseRecomPane/ExerciseRecomPane.js
+++ FrontEnd_DiaTracker/react-script/src/Components/ExerciseRecomPane/ExerciseRecomPane.js
@@ -6,29 +6,79 @@ import getExerciseData from "./ExerciseRecomData.js";
 export default function ExerciseRecomPane({ resentValue }) {
   // const exerciseData = getExerciseData(resentValue);
   // console.log(exerciseData);
-  const [exerciseData, setExerciseData] = useState();
+  console.log(resentValue);
+  const [exerciseData, setExerciseData] = useState([]);
 
   useEffect(() => {
-    if (resentValue > 0){
-      const fetchData = async() => {
-        const data = await - getExerciseData(resentValue);
-        setExerciseData(data.exercise_data);
-      };
-      fetchData();
+    getExerciseData();
+  }, []);
+
+  // Attach a click event listener to the button
+  const getExerciseData = (resentValue) => {
+    if (resentValue > 0) {
+      var cluster;
+      if (resentValue > 250) {
+        cluster = 0;
+      } else if (resentValue > 199) {
+        cluster = 1;
+      } else if (resentValue > 159) {
+        cluster = 2;
+      } else if (resentValue > 139) {
+        cluster = 3;
+      } else if (resentValue > 125) {
+        cluster = 4;
+      } else if (resentValue > 99) {
+        cluster = 5;
+      } else if (resentValue > 69) {
+        cluster = 6;
+      } else {
+        cluster = 7;
+      }
+      console.log(cluster);
+      // Make an HTTP GET request to the Flask API endpoint with the cluster input value as a query parameter
+      return fetch(`http://localhost:5000/exercise_data?cluster=${cluster}`)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          // Display the filtered exercise and food data
+          setExerciseData(data.exercise_data);
+          // return data.exercise_data;
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
     }
-  }, [resentValue])
+  };
+
+ 
+
+  // const fetchData = () => {
+  //   if (resentValue > 0) {
+  //     const fetchData = async () => {
+  //       const data = await -getExerciseData(resentValue);
+        
+  //     };
+  //   }
+  // };
+
   return (
     <div className="ExRecomPane">
       Exercise Recoomendation
       {exerciseData.map((exercise, index) => (
         <Card>
-        <Card.Title>{exercise["Activity (1H)"]}</Card.Title>
-        <Card.Body>
-          <Card.Title>
-            <Card.Text>Calories per kg: {exercise["Calories per kg"]}</Card.Text>
-          </Card.Title>
-        </Card.Body>
-      </Card>
+          <Card.Title>{exercise["Activity (1H)"]}</Card.Title>
+          <Card.Body>
+            <Card.Title>
+              <Card.Text>
+                Calories per kg: {exercise["Calories per kg"]}
+              </Card.Text>
+            </Card.Title>
+          </Card.Body>
+        </Card>
       ))}
     </div>
   );
