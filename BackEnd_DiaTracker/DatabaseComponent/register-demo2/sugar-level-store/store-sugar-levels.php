6b67eaa1563e8f5bbe0ba0ad7d93bff78b7dbcc0
PansiluAW <pansiluwijesiri@gmail.com>
2023-04-03 23:46:41 +0530

    Update store-sugar-levels.php

--- BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/store-sugar-levels.php
+++ BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/store-sugar-levels.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Credentials: true");
  
 session_start();
