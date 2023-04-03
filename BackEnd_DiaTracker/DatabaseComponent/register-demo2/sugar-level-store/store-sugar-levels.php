0451c78e6cd896caaf2c2ff81b6952bca6e7b753
PansiluAW <pansiluwijesiri@gmail.com>
2023-04-03 21:26:03 +0530

    Added code to up the get sugar level code

--- BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/store-sugar-levels.php
+++ BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/store-sugar-levels.php
@@ -1,9 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Credentials: *");
-
+header("Access-Control-Allow-Credentials: true");
+ 
 session_start();
 include('database_connection.php');
 if (isset($_SESSION['verified_user_id'])){
