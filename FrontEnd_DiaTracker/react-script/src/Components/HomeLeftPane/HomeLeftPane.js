7fa647ef43ea0d1fa9d998ccd2ef5ab854a2579a
PansiluAW <pansiluwijesiri@gmail.com>
2023-04-05 01:49:49 +0530

    Updated code

--- FrontEnd_DiaTracker/react-script/src/Components/HomeLeftPane/HomeLeftPane.js
+++ FrontEnd_DiaTracker/react-script/src/Components/HomeLeftPane/HomeLeftPane.js
@@ -51,7 +51,7 @@ export default function HomeLeftPane({ data, setData }) {
     try {
       //push the user input sugar level as an array to the database using api link
       const newRecord = [date, currentInput];
-      await axios.post(
+      axios.post(
         "http://localhost/diaTracker-project/BackEnd_DiaTracker/DatabaseComponent/register-demo2/sugar-level-store/store-sugar-levels.php",
         { date: date, currentInput: currentInput },
         {withCredentials: true}
