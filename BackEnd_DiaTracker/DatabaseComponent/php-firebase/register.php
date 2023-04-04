032bf91d24629e4dd045a2a735ee1c4f88185df5
Chalana <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-04-04 18:05:01 +0530

    Update register.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/register.php
+++ BackEnd_DiaTracker/DatabaseComponent/php-firebase/register.php
@@ -6,13 +6,13 @@ session_start();
 // include the header file, which contains the HTML header and navigation
 include('includes/header.php');
 ?>
-        <div class="loader"></div>
-        <div class="mainbox">
-        <img src="diatracker-logo-transparent.png" alt="diatracker-logo" width="600" class="logo">
-        </div>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class ="col-md-5 mt-5">
+        echo '<div class="loader"></div>';
+        echo '<div class="mainbox">';
+        echo '<img src="diatracker-logo-transparent.png" alt="diatracker-logo" width="600" class="logo">';
+        echo "</div>";
+        echo '<div class="container">';
+        echo '<div class="row justify-content-center">';
+        echo '<div class ="col-md-5 mt-5">';
         
         <?php
                 // display any password error messages
@@ -23,59 +23,59 @@ include('includes/header.php');
                 }
                 ?>
     
-        <div class="box">
-            <div class="card-header">
+        echo '<div class="box">';
+            echo '<div class="card-header">';
                 <h1>Register here!</h1>
  
-        </div>
-        <div class="card-body">
-            <form action="code.php" method="POST">
+        echo "</div>";
+        echo '<div class="card-body">';
+            echo '<form action="code.php" method="POST">';
                 
             
-                <div class="myform2">
-                <p>Username</p>
-                    <input type="text" name="username" class="form-control" placeholder="Enter Username" required="">
-                </div>
-                <br>
-                <div class="form-group-mb-3">
-                <p>Email</p>  
-                    <input type="email" name="email" class="form-control" placeholder="Enter Email Address" required="">
-                </div>
-                <br>
-                <div class="form-group-mb-3">
-                <p>Password</p>
-                    <input type="password" name="password1" class="form-control" placeholder="Enter Password" required pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$" title="Password must have at least one digit, one uppercase letter, one non-alphanumeric character, and be at least 8 characters long">
-                </div>
-                <br>
-                <div class="form-group-mb-3">
-                <p>Retype Password</p>
-                    <input type="password" name="password2" class="form-control" placeholder="Confirm Password" required ="">
-                </div>
+                echo '<div class="myform2">';
+                echo "<p>Username</p>";
+                echo '<input type="text" name="username" class="form-control" placeholder="Enter Username" required="">';
+                echo"</div>";
+                echo '<br>";
+                echo '<div class="form-group-mb-3">';
+                echo "<p>Email</p>";  
+                echo '<input type="email" name="email" class="form-control" placeholder="Enter Email Address" required="">';
+                echo "</div>";
+                echo "<br>";
+                echo '<div class="form-group-mb-3">';
+                echo '<p>Password</p>';
+                echo '<input type="password" name="password1" class="form-control" placeholder="Enter Password" required pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$" title="Password must have at least one digit, one uppercase letter, one non-alphanumeric character, and be at least 8 characters long">';
+                echo "</div>";
+                echo "<br>";
+                echo '<div class="form-group-mb-3">';
+                echo "<p>Retype Password</p>";
+                echo '<input type="password" name="password2" class="form-control" placeholder="Confirm Password" required ="">';
+                echo "</div>";
                 <?php 
                 if(isset($_SESSION['pwd_error'])) { ?>
-                <div class="alert alert-danger">
+                echo '<div class="alert alert-danger">';
                     <?php echo $_SESSION['pwd_error']; unset($_SESSION['pwd_error']); ?>
-                </div>
+                echo '</div>';
                 <?php } ?>
-                <br>
+                echo "<br>";
                  // Add reCAPTCHA widget
                  echo '<div class="g-recaptcha" data-sitekey="6Le40FklAAAAABRczZ6GGVMI-0Mxot82OtlM3YJy"> </div>';                        
                  echo "<br>";
-                <div class="myform2 ">
-                 <input type="submit" name="register_now_btn" class="btn btn-primary"value="Register" ></input>
-                </div>
+                 echo '<div class="myform2 ">';
+                 echo '<input type="submit" name="register_now_btn" class="btn btn-primary"value="Register" ></input>';
+                echo "</div>";
                 
-                <br>
-                <p class="text-center">Already have an account? <a href="login.php">Sign in</a></p>
+                echo "<br>";
+                echo '<p class="text-center">Already have an account? <a href="login.php">Sign in</a></p>';
 
-            </form>
+            echo "</form>";
                 
-            <script src="loading.js"></script>
+            echo '<script src="loading.js"></script>';
             echo '<script src="https://www.google.com/recaptcha/api.js" async defer></script>';
-        </div>    
-    </div>
-    </div>
-</div>
+        echo "</div>";    
+    echo "</div>";
+    echo "</div>";
+echo "</div>";
 
 <?php
 // include the footer file, which contains the closing HTML tags
