bb4f2b9035928bd849d5b9f457265c195fdf2b1e
Chalana <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-04-04 13:56:52 +0530

    Update logout.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php
+++ BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
 
+// unset the session variables for the authenticated user
 unset($_SESSION['verified_user_id']);
 unset($_SESSION['idTokenString']);
 
+// set the session variable to indicate that the user has been logged out
 $_SESSION['status'] = "<div class='error'>Logged Out Successfully</div>";
+
+// redirect the user to the login page
 header("Location: login.php");
 exit();
 ?>
