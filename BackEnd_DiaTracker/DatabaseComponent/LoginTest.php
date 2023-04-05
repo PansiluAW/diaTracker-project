ae9f22055706c2c2d6b90b33c391f253778cd5b8
Chalana <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-04-05 20:36:59 +0530

    Add files via upload

---
+++ BackEnd_DiaTracker/DatabaseComponent/LoginTest.php
@@ -0,0 +1,68 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class LoginTest extends TestCase
+{
+    private $client;
+    
+    public function setUp(): void
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://localhost/'
+        ]);
+    }
+    
+    public function testLoginPage()
+    {
+        $response = $this->client->get('/login.php');
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringContainsString('Login', (string)$response->getBody());
+    }
+
+    public function testInvalidEmail()
+    {
+        $response = $this->client->post('/login.php', [
+            'form_params' => [
+                'email' => 'invalid_email',
+                'password' => 'password',
+                'g-recaptcha-response' => 'valid_captcha_response'
+            ]
+        ]);
+        $this->assertSame(302, $response->getStatusCode());
+        $this->assertStringContainsString('No Email Found', $_SESSION['status']);
+    }
+
+    public function testInvalidPassword()
+    {
+        $response = $this->client->post('/login.php', [
+            'form_params' => [
+                'email' => 'valid_email@example.com',
+                'password' => 'invalid_password',
+                'g-recaptcha-response' => 'valid_captcha_response'
+            ]
+        ]);
+        $this->assertSame(302, $response->getStatusCode());
+        $this->assertStringContainsString('Invalid Email or Password', $_SESSION['status']);
+    }
+
+    public function testValidLogin()
+    {
+        $response = $this->client->post('/login.php', [
+            'form_params' => [
+                'email' => 'valid_email@example.com',
+                'password' => 'valid_password',
+                'g-recaptcha-response' => 'valid_captcha_response'
+            ]
+        ]);
+        $this->assertSame(302, $response->getStatusCode());
+        $this->assertStringContainsString('You are Logged in Successfully', $_SESSION['status']);
+        $this->assertStringContainsString('User ID:', (string)$response->getBody());
+        $this->assertStringContainsString('Location: http://localhost:3000', implode(', ', $response->getHeader('Location')));
+    }
+    
+    public function tearDown(): void
+    {
+        $_SESSION = [];
+    }
+}
