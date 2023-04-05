1ad7ef7e8299e06f0e9a4b85942a11c1ef4d8aa3
Methuu1119 <104524747+Methuu1119@users.noreply.github.com>
2023-04-05 13:56:04 +0530

    NavBar.js file updated

--- FrontEnd_DiaTracker/react-script/src/Components/Navigation/NavBar.js
+++ FrontEnd_DiaTracker/react-script/src/Components/Navigation/NavBar.js
@@ -3,19 +3,10 @@ import "./navBar.css";
 import diatraclogo from "./diatraclogo.png";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import About from "../../Pages/About/About.js";
-// import HomeScreen from "../../Pages/HomeScreen/HomeScreen";
-// import About from "./FrontEnd_DiaTracker/About.html";
-// import LogOut from "./BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php";
 
 export default function NavBar() {
   return (
     <>
-      {/* <div class="navbar">
-          <Route path="/" component={HomeScreen} />
-          <Route path="/FrontEnd_DiaTracker/About.html" component={About} />
-        </div> */}
-
       <nav>
         <div class="logo">
           <img src={diatraclogo} alt="Diatrac logo" />
@@ -25,13 +16,9 @@ export default function NavBar() {
           <li>
             <a href="/">Home</a>
           </li>
-          {/* <li>
-          <Route path="/Pages/About/About.js" component={About} />
-          </li> */}
+
           <li>
-            <a href="">
-              About Us
-            </a>
+            <a href="">About Us</a>
           </li>
           <li>
             <a href="http://localhost/diaTracker-project/BackEnd_DiaTracker/DatabaseComponent/php-firebase/logout.php">
