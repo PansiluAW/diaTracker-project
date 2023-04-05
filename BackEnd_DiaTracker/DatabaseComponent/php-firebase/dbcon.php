c199e0a01fd52279bd349f9a416c3faff4965280
Chalana <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-04-05 15:40:54 +0530

    Update dbcon.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/dbcon.php
+++ BackEnd_DiaTracker/DatabaseComponent/php-firebase/dbcon.php
@@ -9,6 +9,7 @@ $factory = (new Factory)
 ->withServiceAccount('fir-project-5a710-firebase-adminsdk-hwnsw-9199d08e80.json')
 ->withDatabaseUri('https://fir-project-5a710-default-rtdb.firebaseio.com/');
 
+// Create a database reference and an authentication reference
 $database = $factory->createDatabase();
 $auth = $factory->createAuth();
 
