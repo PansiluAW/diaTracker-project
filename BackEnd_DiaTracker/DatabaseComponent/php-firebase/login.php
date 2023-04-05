986b9bc1d3016f5133972425bd09576eb68bc02d
Chalana <101565998+ChalanaSangeeth123@users.noreply.github.com>
2023-04-05 10:04:41 +0530

    Update login.php

--- BackEnd_DiaTracker/DatabaseComponent/php-firebase/login.php
+++ BackEnd_DiaTracker/DatabaseComponent/php-firebase/login.php
@@ -3,7 +3,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE. OPTIONS");
 header("Access-Control-Allow-Credentials: true");
-session_start();  
+session_start(); 
+
+
+// Check if the user is already logged in
 if(isset($_SESSION['verified_user_id'])) {
     $_SESSION['status'] = "You are logged in";
 
